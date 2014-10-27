abebdd6f18895010fbab7ebfb4cebf620b7e6855
Stanislas H.B. Sodonon <bidossessi@linuxbenin.com>
2014-10-27 23:37:29 +0100

    argilsoft init

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+{
+    "name" : "Loan Management",
+    "version" : "1.0", 
+    "category" : "HR", 
+    "complexity" : "normal", 
+    "author" : "ColourCog.com", 
+    "website" : "http://colourcog.com", 
+    "depends" : ["base", "hr", "hr_contract", "hr_expense"], 
+    "summary" : "Management for Employee Loans", 
+    "description" : "Management for Employee Loans",
+    "data" : [ 
+      'security/hr_loan_security.xml', 
+      'hr_expense_loan_view.xml', 
+      'hr_expense_loan_sequence.xml', 
+    ], 
+    "application": False, 
+    "installable": True
+}
+
